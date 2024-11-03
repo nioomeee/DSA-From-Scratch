ff1a875a4b80976d03e676380b4580733c5e5836
nioomeee <nioomeee@gmail.com>
2024-11-03 22:19:39 +0530

    index of the smallest element also pushed

--- arrays/smallest.cpp
+++ arrays/smallest.cpp
@@ -6,12 +6,21 @@ int main(){
     int arr[]={3, 6, 1, 5, 8 , 2, 0, -8, 3, -25};
     int size = sizeof(arr)/sizeof(arr[0]);
     int smallest=arr[0];
+    int idx = 0;
 
     for(int i = 0; i < size; i ++){
         smallest = min(arr[i], smallest);
     }
 
+    // Returing index of the smallest element
+    for(int i = 0; i < size; i ++){
+        if(smallest = arr[i]){
+            idx = i;
+        }
+    }
+
     cout<<"\n Smallest number = "<<smallest<<endl;
+    cout<<"\n Index of the smallest element = "<<idx<<endl;
 
     return 0;
 }
