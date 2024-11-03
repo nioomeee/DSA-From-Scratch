b87fb5b6d275e5b3244a0401ea757b39e7116213
nioomeee <nioomeee@gmail.com>
2024-11-03 22:24:59 +0530

    index of largest pushed

--- arrays/largest.cpp
+++ arrays/largest.cpp
@@ -5,11 +5,17 @@ int main(){
     int arr[]={3, 6, 1, 5, 8 , 2, 0, -8, 3, -25};
     int size = sizeof(arr)/sizeof(arr[0]);
     int largest = arr[0];
+    int idx= 0;
 
     for(int i = 0; i < size; i ++){
-        largest = max(arr[i], largest);
+        if(arr[i] > largest){
+            largest  = arr[i];
+            idx = i;
+        }
+
     }
 
     cout<<"\n Largest = "<<largest<<endl;
+    cout<<"\n Index of maximum element = "<<idx;
     return 0;
 }
