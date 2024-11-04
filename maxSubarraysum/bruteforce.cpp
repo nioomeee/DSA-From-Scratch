b26d5d0f559991302189a738cdc56b10aea27476
nioomeee <nioomeee@gmail.com>
2024-11-05 00:36:47 +0530

    brute frce approach O(n^2)

--- maxSubarraysum/bruteforce.cpp
+++ maxSubarraysum/bruteforce.cpp
@@ -2,17 +2,19 @@
 using namespace std;
 
 int main(){
-    int n = 5;
-    int arr[]={1, 2, 3, 4, 5};
+    int n = 7;
+    int arr[]={3, -4, 5, 4, -1, 7, -8};
+
+    int maxSum = 0;
 
     for(int start = 0; start < n; start ++){
+        int currsum = 0;
         for(int end = start; end < n; end ++){
-            for(int i = start; i <= end; i ++){
-                
-            }
-            cout<<" ";
+            currsum += arr[end];
+            maxSum = max(currsum, maxSum);
         }
-        cout<<endl;
     }
+
+    cout<<"\n Maximum subarrya sum = "<<maxSum<<endl;
     return 0;
 }
