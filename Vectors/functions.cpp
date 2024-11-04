c9d794f19d1c65e441430c0d290a5ac2e05aa779
nioomeee <nioomeee@gmail.com>
2024-11-04 23:01:45 +0530

    size of vector

---
+++ Vectors/functions.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+int main(){
+    vector <int> vec = {1, 2, 3, 4};
+
+    // size function
+    cout<<"\n Size of vector = "<<vec.size()<<endl;
+    return 0;
+}
