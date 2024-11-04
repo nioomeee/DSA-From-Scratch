a8e5010dc8f0bc5ae8959e0e93a1fc519c727c63
nioomeee <nioomeee@gmail.com>
2024-11-04 23:10:47 +0530

    front

--- Vectors/functions.cpp
+++ Vectors/functions.cpp
@@ -9,12 +9,16 @@ int main(){
     cout<<"\n Size of vector before push back = "<<vec1.size()<<endl;
     vec1.push_back(25);
     vec1.push_back(35);
+    vec1.push_back(85);
     vec1.push_back(65); //push back 
     cout<<"\n Size of vector after push back = "<<vec1.size()<<endl;
 
     // pop back
     vec1.pop_back();
     cout<<"\n Size of vector after pop back = "<<vec1.size()<<endl;
+
+    // front
+    cout<<"\n Front (1st value) of vector = "<<vec1.front();
 
 
     for(int val : vec1){
