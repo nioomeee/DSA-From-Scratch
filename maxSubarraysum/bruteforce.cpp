00ad3dbc9a5f3ee78c008d6c95b3118b3e09026f
nioomeee <nioomeee@gmail.com>
2024-11-05 00:28:31 +0530

    brute force approach

--- maxSubarraysum/bruteforce.cpp
+++ maxSubarraysum/bruteforce.cpp
@@ -8,7 +8,7 @@ int main(){
     for(int start = 0; start < n; start ++){
         for(int end = start; end < n; end ++){
             for(int i = start; i <= end; i ++){
-                cout<<arr[i];
+                
             }
             cout<<" ";
         }
