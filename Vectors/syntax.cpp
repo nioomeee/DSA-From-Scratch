2ea267799d7a8cb2a72929181153bb7f71f06fb7
nioomeee <nioomeee@gmail.com>
2024-11-04 19:58:36 +0530

    the basic syantax of vectors

---
+++ Vectors/syntax.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+#include <vector>
+using namespace std;
+
+int main(){
+    vector<int> vec1 = {1, 2, 3}; //like arrays
+    vector<int> vec2(3, 0); //creates a vector of size 3 with all elements as 0
+    cout<<vec1[0]<<endl;
+    cout<<vec2[0]<<endl;
+    return 0;
+}
