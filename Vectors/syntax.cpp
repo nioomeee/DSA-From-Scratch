cc60569ff62fbb6cc679cddb007aaffa28f71c1c
nioomeee <nioomeee@gmail.com>
2024-11-04 19:58:47 +0530

    basic syntax of vectors

--- Vectors/syntax.cpp
+++ Vectors/syntax.cpp
@@ -5,7 +5,11 @@ using namespace std;
 int main(){
     vector<int> vec1 = {1, 2, 3}; //like arrays
     vector<int> vec2(3, 0); //creates a vector of size 3 with all elements as 0
+    vector<int> vec3; //creates a vector of size 0
+    
+
     cout<<vec1[0]<<endl;
     cout<<vec2[0]<<endl;
+
     return 0;
 }
