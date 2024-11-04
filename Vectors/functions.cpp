ce497a67a19011e3612eb2076f38b2916b42bf33
nioomeee <nioomeee@gmail.com>
2024-11-04 23:08:36 +0530

    pop back

--- Vectors/functions.cpp
+++ Vectors/functions.cpp
@@ -8,7 +8,14 @@ int main(){
    
     cout<<"\n Size of vector before push back = "<<vec1.size()<<endl;
     vec1.push_back(25);
+    vec1.push_back(35);
+    vec1.push_back(65); //push back 
     cout<<"\n Size of vector after push back = "<<vec1.size()<<endl;
+
+    // pop back
+    vec1.pop_back();
+    cout<<"\n Size of vector after pop back = "<<vec1.size()<<endl;
+
 
     for(int val : vec1){
         cout<<"\n " << val;
