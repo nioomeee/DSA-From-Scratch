4ba7fc6cb97d5576db941cd25832c8ef35b49520
nioomeee <nioomeee@gmail.com>
2024-11-05 12:34:51 +0530

    changed the array to vector

--- PairSum/bruteforce.cpp
+++ PairSum/bruteforce.cpp
@@ -5,16 +5,16 @@ using namespace std;
 
 int main(){
     
-    int arr[] = {2, 7, 11, 15};
+    vector <int> nums = {2, 7, 11, 15};
     int target = 9;
     vector <int> vec;
-    int n = sizeof(arr)/sizeof(arr[0]);
+    int n = sizeof(nums)/sizeof(nums[0]);
 
     for(int i = 0; i < n; i++){
         for(int j = i+1; j < n ; j ++){
-            if(arr[i]+arr[j] == target){
-                vec.push_back(arr[i]);
-                vec.push_back(arr[j]);
+            if(nums[i]+nums[j] == target){
+                vec.push_back(nums[i]);
+                vec.push_back(nums[j]);
             } 
         }
     }
