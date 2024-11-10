45c6fda0c258aeb3908cb61cb70362a86a0e916f
nioomeee <nioomeee@gmail.com>
2024-11-10 16:32:43 +0530

    using '*' operator for poinyter variables

--- Pointer/basic.cpp
+++ Pointer/basic.cpp
@@ -5,5 +5,8 @@ int main(){
     
     int a = 10;
     cout<<"\n Printing the address using address of operator  = "<< &a <<endl;
+
+    int* ptr = &a;
+    cout<<"\n Printing address using * and pointer variable = "<<ptr<<endl;
     return 0;
 }
