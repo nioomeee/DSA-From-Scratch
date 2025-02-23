5ba689eec5b9317e76b16dd2184c844101998edf
nioomeee <nioomeee@gmail.com>
2025-02-23 15:13:19 +0530

    search in LL-very ezzy

--- Linked Lists/searchInLL.cpp
+++ Linked Lists/searchInLL.cpp
@@ -33,12 +33,53 @@ public:
         }
     }
 
+    void searchLL(int val){
+
+        Node* temp = head;
+        int count = 0;
+        while( temp != NULL ){
+
+            if(temp->data == val){
+                cout<<"\n Value "<<val<<" found at position "<<count<<endl;
+                return;
+            }
+            temp = temp->next;
+            count++;
+        }
+
+        cout<<"\n Value "<<val<<" not found in the linked list"<<endl;
+        return;
+    }
+
+    void printLL() {
+        Node* temp = head;
+
+        if ( head == NULL ) {
+            cout<<"\n the linked list is empty"<<endl;
+            return;
+        } else {
+            while (temp != NULL){
+                cout<<temp->data<<" ";
+                temp = temp->next;
+            }
+            cout<<"NULL"<<endl;
+        }
+    }
     
 };
 
 int main(){
     
-    
+    List ll;
+    ll.push_front(4);
+    ll.push_front(3);
+    ll.push_front(2);
+    ll.push_front(1);
+
+    ll.printLL();
+
+    ll.searchLL(2);
+    ll.searchLL(6);
     cout<<endl;
     return 0;
 }
