1895a010962cc4a90ebd3d2552fcacf51f9b128d
nioomeee <nioomeee@gmail.com>
2025-02-23 15:03:43 +0530

    push_front function

---
+++ Linked Lists/searchInLL.cpp
@@ -0,0 +1,44 @@
+#include<iostream>
+using namespace std;
+
+class Node{
+public:
+    int data;
+    Node* next;
+
+    Node(int val){
+        data = val;
+        next = NULL;
+    }
+};
+
+class List{
+    
+    Node* head;
+    Node* tail;
+
+public:
+    List(){
+        head = tail = NULL;
+    }
+
+    void push_front(int val){
+        Node* newNode = new Node(val);
+        if(head == NULL){
+            head = tail = newNode;
+            return;
+        } else {
+            newNode-> next = head;
+            head = newNode;
+        }
+    }
+
+    
+};
+
+int main(){
+    
+    
+    cout<<endl;
+    return 0;
+}
