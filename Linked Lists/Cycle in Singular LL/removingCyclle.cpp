5b68c6a86e1597431be5bf539a7fec6b0fb250e3
nioomeee <nioomeee@gmail.com>
2025-02-23 18:35:42 +0530

    removing cycle in linked list

---
+++ Linked Lists/Cycle in Singular LL/removingCyclle.cpp
@@ -0,0 +1,132 @@
+// Q 142 - Leetcode
+#include<iostream>
+using namespace std;
+
+class Node{
+public:
+
+    int data;
+    Node* next;
+
+    Node(int val){
+        data = val;
+        next = NULL;
+    }
+};
+
+class List{
+
+    Node* head;
+    Node* tail;
+
+public:
+    List(){
+        head = tail = NULL;
+    }
+
+    void push_front(int val){
+        Node* newNode = new Node(val);
+        if(head == NULL){
+            head = tail = newNode;
+            return;
+        } else {
+            newNode->next = head;
+            head = newNode;
+        }
+    }
+
+    bool detectCycle(){
+        Node* slow = head;
+        Node* fast = head;
+
+        while ( fast != NULL && fast->next != NULL ){
+            slow = slow->next;
+            fast = fast->next->next;
+
+            if( slow == fast ){
+                return true;
+            }
+        }
+        return false;
+    }
+
+    void removeCycle(){
+        Node* slow = head;
+        Node* fast = head;
+
+        bool isCycle = false;
+
+        while( fast != NULL && fast->next != NULL ){
+            slow = slow->next;
+            fast = fast->next->next;
+
+            if(slow == fast){
+                isCycle = true;
+                break;
+            }
+        }
+
+        if(!isCycle){
+            return;
+        }
+
+        slow = head;
+
+        Node* prev = NULL;
+        while( slow != fast ){
+            slow = slow->next;
+            prev = fast;
+            fast = fast->next;
+        }
+        prev->next = NULL; //removing the cycle
+    }
+
+    void printLL(){
+        if(head == NULL){
+            cout<<"\n the linked list is empty"<<endl;
+        } else {
+            Node* temp = head;
+
+            while (temp != NULL){
+                cout<<temp->data<<" -> ";
+                temp = temp->next;
+            }
+        }
+        cout<<"NULL"<<endl;
+    }
+};
+
+int main(){
+    
+    List ll;
+    ll.push_front(5);
+    ll.push_front(4);
+    ll.push_front(3);
+    ll.push_front(2);
+    ll.push_front(1);
+
+    cout << "\nOriginal Linked List:" << endl;
+    ll.printLL();
+
+    cout << "\nFor odd number (5):" << endl;
+    if(ll.detectCycle()){
+        cout<<"\n Cycle detected"<<endl;
+    } else {
+        cout<<"\n Cycle not detected"<<endl;
+    }
+
+    ll.push_front(6);
+    cout << "\nUpdated Linked List:" << endl;
+    ll.printLL();
+
+    cout << "\nFor even number (6):" << endl;
+    if(ll.detectCycle()){
+        cout<<"\n Cycle detected"<<endl;
+    } else {
+        cout<<"\n Cycle not detected"<<endl;
+    }
+
+
+    cout<<endl;
+    return 0;
+}
