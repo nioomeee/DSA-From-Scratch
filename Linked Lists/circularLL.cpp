251d932b33684816df7115e22d0b01ba72d5067c
nioomeee <nioomeee@gmail.com>
2025-02-23 21:19:26 +0530

    delete at Head in Circular LL

--- Linked Lists/circularLL.cpp
+++ Linked Lists/circularLL.cpp
@@ -33,6 +33,46 @@ public:
         }
     }
 
+    void insertTail(int val) {
+        Node* newNode = new Node(val);
+
+        if(tail == NULL) {
+            head = tail = newNode;
+            tail->next = head;
+        } else {
+            newNode->next = head;
+            tail->next = newNode;
+            tail = newNode;
+        }
+    }
+
+    void delHead() {
+        if(head == NULL) {
+            cout << "the linked list is empty" << endl;
+            return;
+        } else if(head == tail) {
+            delete head;
+            head = tail = NULL;
+        } else {
+            Node* temp = head;
+            head = head->next;
+            tail->next = head;
+            
+            temp->next = NULL;
+            delete temp;
+        }
+    } 
+
+    void delTail() {
+        if(head == NULL) {
+            cout << "the linked list is empty" << endl;
+            return;
+        } else {
+            Node* temp = tail;
+            
+        }
+    }
+
     void print() {
         if(head == NULL) {
             cout << "\n The linked list is empty" << endl;
@@ -61,6 +101,14 @@ int main() {
     cll.insertHead(1);
 
     cout << "The original linked list: " << endl;
+    cll.print();
+
+    cll.insertTail(420);
+    cout << "The updated linked list: " << endl;
+    cll.print();
+
+    cll.delHead();
+    cout << "The updated linked list after deleting at head: " << endl;
     cll.print();
     cout<<endl;
     return 0;
