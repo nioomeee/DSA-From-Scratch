c5576f75d008a90cfdd0fd7dad640b6b452cecf4
nioomeee <nioomeee@gmail.com>
2025-03-19 18:30:16 +0530

    stack with arrays

---
+++ Stacks/witharray.cpp
@@ -0,0 +1,70 @@
+#include<iostream>
+using namespace std;
+
+class Stack{
+    int arr[10];    
+    int topindex;
+
+public:
+    Stack() {
+        topindex = -1;
+    } 
+
+    void pushval(int val) {
+
+        if(topindex >= 9) {
+            cout << "Stack overflow" << endl;
+            return;
+        }
+        arr[++topindex] = val;
+    }
+
+    void pop() {
+
+        if(topindex < 0) {
+            cout << "Stack underflow" << endl;
+            return;
+        }
+
+        topindex--;
+    }
+
+    int top() {
+
+        if(topindex < 0) {
+            cout << "Stack is empty" << endl;
+            return -1;
+        }
+
+        return arr[topindex];
+    }
+
+    bool isEmpty() {
+        return topindex == -1; //returns true if stack is empty
+    }
+};
+
+int main(){
+    
+    Stack s;
+
+    s.pushval(10);
+    s.pushval(20);
+    s.pushval(30);
+
+    cout << "Top element = " <<s.top() << endl;;
+
+    s.pop();
+    cout << "Top element after pop = " << s.top() << endl;;
+
+    s.pop();
+    s.pop();
+    s.pop(); //should print stack underflow
+
+    cout<<endl;
+
+    cout << (s.isEmpty() ? "Stack is empty" : "Stack is not empty") << endl;
+
+    cout<<endl;
+    return 0;
+}
